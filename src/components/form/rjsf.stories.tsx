1e1c0328fcf067da0068d05bad5fb45e9a858494
Ali Ghorbani <alidoubleplus@gmail.com>
2023-06-02 22:54:21 +0300

    samples

--- src/components/form/rjsf.stories.tsx
+++ src/components/form/rjsf.stories.tsx
@@ -1,37 +1,44 @@
-import React from "react";
-import { Meta, StoryObj } from "@storybook/react";
-import  Form  from "./rjsf.wahab";
-import { Type } from '@sinclair/typebox'
-import validator from "@rjsf/validator-ajv8";
+import React, { ComponentProps } from "react"
+import { Meta, StoryObj } from "@storybook/react"
+import Form from "./rjsf.wahab"
+import { Type } from "@sinclair/typebox"
+import validator from "@rjsf/validator-ajv8"
+import {samples, Sample} from "./samples"
 
-const meta: Meta<typeof Form> = {
-  title: 'Components/rjsf',
-  tags: ['autodocs'],
-  render(args) {
-    return (<Form {...args} validator={validator} />)
+const meta: Meta<ComponentProps<typeof Form> & { sample?: Sample }> = {
+  title: "Components/wahab-jsf",
+  render (args) {
+    const sampleArgs = args.sample ? samples[args.sample] : {}
+
+    return (<Form {...args} {...sampleArgs} validator={validator} />)
   },
   argTypes: {
-    schema: { control: 'object' },
-    formData: { control: 'object' },
-    onChange: { action: 'change' },
-    onSubmit: { action: 'submit' },
-    onError: { action: 'error' },
+    sample: { control: "select", options: Object.keys(samples) },
+    schema: { control: "object" },
+    formData: { control: "object" },
+    onChange: { action: "change" },
+    onSubmit: { action: "submit" },
+    onError: { action: "error" },
   }
 }
 
-export default meta;
+export default meta
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
   args: {
     schema: Type.Object(
       {
-        title: Type.String({ title: 'Title' }),
+        string: Type.String({ title: "Title", description: "Title field description" }),
+        number: Type.Number({ title: "Age", description: "Age field description" }),
+        date: Type.String({ title: "Birthdate", format: "date-time", description: "Birthdate field description" }),
+        boolean: Type.Boolean({ title: "Is Admin", description: "Is Admin field description" }),
+        integer: Type.Integer({ title: "Books count", description: "Books count description" }),
       },
-      { $id: 'Sample', additionalProperties: false, title: 'Single text field' }
+      { $id: "Sample", additionalProperties: false, title: "Single text field", description: "form description" }
     ),
     formData: {
-      title: "inittial title"
+      string: "inittial title"
     },
   }
 }
--- src/components/form/rjsf.wahab.tsx
+++ src/components/form/rjsf.wahab.tsx
@@ -1,3 +1,44 @@
-import Form from "@rjsf/core";
+import React from "react"
+// import Form from "@rjsf/core"
+import { withTheme, ThemeProps, } from "@rjsf/core"
+import { Box } from "../../atoms/box/Box"
+import { ColorProp } from "../../styled/types"
+import { Text } from "../../atoms/text/Text"
 
-export default Form
+function withBoxWrapper (name: string, color?: ColorProp) {
+  const WrappedBox = (props: React.PropsWithChildren) => {
+    console.log("mkBoxWrapper", name, props)
+
+    return (
+      <Box border={color && { s: 1, c: color }}>
+        <Text bg={color} padding={{x: "s"}}>{name}</Text>
+        <Box {...props} padding={{ all: "s" }}>
+          {props.children}
+        </Box>
+      </Box>
+    )
+  }
+
+  return WrappedBox
+}
+
+const theme: ThemeProps = {
+  // fields: {
+  //   NumberField: withBoxWrapper("F:NumberField", "danger"),
+  //   TitleField: withBoxWrapper("F:TitleField", "danger"),
+  //   StringField: withBoxWrapper("F:StringField", "danger"),
+  //   BooleanField: withBoxWrapper("F:BooleanField", "danger"),
+  // },
+  // templates: {
+  //   BaseInputTemplate: withBoxWrapper("T:BaseInputTemplate", "danger"),
+  //   DescriptionFieldTemplate: withBoxWrapper("T:DescriptionFieldTemplate", "danger"),
+  // },
+  // widgets: {
+  //   TextWidget: withBoxWrapper("W:TextWidget", "danger"),
+  // },
+  _internalFormWrapper: withBoxWrapper("inner", "danger")
+}
+
+const WahabForm = withTheme(theme)
+
+export default WahabForm
