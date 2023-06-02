50881e6210856522f19254a7e8ca3fbb34acf0cf
Ali Ghorbani <alidoubleplus@gmail.com>
2023-06-02 14:41:45 +0300

    lint oin save

--- src/styled/Pallete.stories.tsx
+++ src/styled/Pallete.stories.tsx
@@ -1,9 +1,8 @@
 import React from 'react'
-import type { Meta, StoryObj } from '@storybook/react';
-import {Box} from '../atoms/box/Box';
-import { Theme } from "./theme/theme"
-import {light} from '../styled/theme/theme'
-
+import type { Meta, StoryObj } from '@storybook/react'
+import { Box } from '../atoms/box/Box'
+import { Theme } from './theme/theme'
+import { light } from '../styled/theme/theme'
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
 const meta: Meta<typeof Box> = {
@@ -13,19 +12,19 @@ const meta: Meta<typeof Box> = {
       <div>
         {Object.keys(light.palette).map((v) => (
           <div key={v} style={{ display: 'flex' }}>
-            <Box center bg={v} width={6} height={3}>
+            <Box center={true} bg={v} width={6} height={3}>
               {v}
             </Box>
             <Box width={0.2} height={3}/>
             {Object.keys(light.lightness).map((l) => (
-              <Box center key={l} bg={{v, l}} width={3} height={3}>
+              <Box center={true} key={l} bg={{v, l}} width={3} height={3}>
                 {v === args.color && l === args.light ? 'here' : ''}
               </Box>
             ))}
             <Box width={0.2} height={3}/>
             {['active', 'success', 'warn', 'danger'].includes(v) && (
               <Box
-                center
+                center={true}
                 bg={{ v, a: 'lightest'  }}
                 width={6}
                 height={3}
@@ -44,13 +43,13 @@ const meta: Meta<typeof Box> = {
   },
 } 
 
-export default meta;
-type Story = StoryObj<typeof meta>;
+export default meta
+type Story = StoryObj<typeof meta>
 
 // More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
 export const Palletre: Story = {
   args: {
-    color: "primary",
+    color: 'primary',
     light: null
   },
-};
+}
--- src/styled/cleaned.tsx
+++ src/styled/cleaned.tsx
@@ -1,12 +1,13 @@
-import React, { forwardRef } from "react"
+import React, { forwardRef } from 'react'
 // @ts-ignore
 import cleanProps from 'clean-react-props'
-
 
 const inputOnlyProps = ['value', 'onChange', 'name', 'placeholder'],
   excludeProps = ['height', 'width', 'color', 'default', 'open']
 
+/* tslint:disable-next-line */
 export default function cleaned(Component: React.ComponentType<any>, input: boolean) {
+  /* tslint:disable-next-line */
   const Cleaned = forwardRef<unknown, any>((props, ref) => {
     return (
       <Component
